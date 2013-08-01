501ec1175f6e9ef9faf35abba8e79b311f452661
Timothy Hahn <tyh25@drexel.edu>
2013-08-01 18:23:49 -0400

    Set up views so josh can start

--- inquizition/views.py
+++ inquizition/views.py
@@ -3,36 +3,17 @@ from flask import url_for, redirect, request, render_template, session, flash
 from database import db_session
 from models import Quiz
 
-## HOW TO SIMPLY SHOW A STATIC PAGE
+## Homepage
 @app.route('/')
 def home():
     return redirect(url_for('static', filename='index.html'))
 
-## SIMPLE ROUTE
-@app.route('/hello')
-def hello_world():
-    return 'Hello, world!'
 
-@app.route('/quiz/json', methods=['GET'])
+## API
+@app.route('/quiz', methods=['GET'])
 def list_quiz_json():
-    json = "{"
-    quizzes = Quiz.query.all()
-    for quiz in quizzes:
-        json += '"quiz": '
-        json += '"' + quiz.name +'",'
-    json += "}"
     return json
 
-
-## HOW TO "RECEIVE" DATA AND DB TRANSACTION
-@app.route('/quiz/create', methods=['POST'])
-def create_quiz():
-    quizName = (str)(request.form['qname'])
-    q = Quiz(quizName)
-    db_session.add(q)
-    db_session.commit()    
-    print quizName
-    return '{"status": "OK", "quiz name": "'+quizName+'"}'
 
 ## TURN OFF DB
 @app.teardown_appcontext
