e8e55214e42067c608289182ad86d905e4a35e56
Timothy Hahn <tyh25@drexel.edu>
2013-08-06 11:42:59 -0400

    Created base for views and updated models a bit

--- inquizition/models.py
+++ inquizition/models.py
@@ -1,13 +1,16 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from database import Base
+import json
 
 
 class Quiz(Base):
     __tablename__ = 'quiz'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
+    name = Column(String(50), nullable=False)
     start_time = Column(DateTime)
     end_time = Column(DateTime)
+    questions = Column(String(100)) ## IDs stored as json
+                                    ## For example: {1, 2, 3, 4}
     
     def __init__(self, name=None):
         self.name = name
@@ -18,30 +21,71 @@ class Quiz(Base):
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(80))
+    username = Column(String(80), nullable=False)
+    
+    def __init__(self, name=None):
+        self.name = name
 
-class Responses(Base):
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+
+class Response(Base):
     __tablename__ = 'response'
     id = Column(Integer, primary_key=True)
     answered = Column(Boolean)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    quiz_id = Column(Integer, ForeignKey('quiz.id'))
-    question_id = Column(Integer, ForeignKey('question.id'))
+    question_id = Column(Integer, ForeignKey('question.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    quiz_id = Column(Integer, ForeignKey('quiz.id'), nullable=False)
     user_response = Column(String(1))
-    correct_response = Column(String(1))
+    correct_response = Column(String(1),nullable=False)
     time_elapsed = Column(DateTime)
+
+    def __init__(self, question_id, user_id, quiz_id):
+        this.user_id = user_id
+        this.quiz_id = quiz_id
+        this.answered = False
+        
+    def __repr__(self):
+        return '<Response %d>' % (self.id)
 
 class Result(Base):
     __tablename__ = 'result'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    quiz_id = Column(Integer, ForeignKey('quiz.id'), nullable=False)
     score = Column(Integer)
+    date = Column(DateTime, nullable=False)
 
-class Questions(Base):
+    def __init__(self, user_id, quiz_id, score):
+        this.user_id = user_id
+        this.quiz_id = quiz_id
+        this.score = score
+        this.date = datetime.datetime.now
+        
+    def __repr__(self):
+        return '<Result %d>' % (self.id)
+
+
+class Question(Base):
     __tablename__ = 'question'
     id = Column(Integer, primary_key=True)
+    text = Column(String(120), nullable=False)
     correct_answer = Column(String(120))
     other_answer1 = Column(String(120))
     other_answer2 = Column(String(120))
     other_answer3 = Column(String(120))
 
+    def __init__(self, text):
+        this.text = text
+        
+    def __repr__(self):
+        return '<Question %r>' % (self.text)
+
+    def json(self):
+        question = dict()
+        answers = [correct_answer, other_answer1, other_answer2, other_answer3]
+
+        question['questionText'] = text
+        question['questionID'] = id
+        question['answers'] = answers
+        return json.dumps(question)
--- inquizition/views.py
+++ inquizition/views.py
@@ -3,22 +3,80 @@ from flask import url_for, redirect, request, render_template, session, flash
 from database import db_session
 from models import Quiz
 
-## Homepage
+# Homepage
 @app.route('/')
 def home():
     return redirect(url_for('static', filename='index.html'))
 
 
-## API
+# API
+## GET
 @app.route('/quiz', methods=['GET'])
-def list_quiz_json():
+def list_quizzes():
+    ## List all quizzes
+    ## Convert to json
     return json
 
+@app.route('/quiz/<int:quiz_id>',methods['GET'])
+def find_quiz(quiz_id):
+    ## Find quiz at id
+    ## Convert to json
+    return json
 
-## TURN OFF DB
+@app.route('/quiz/results/<int:quiz_id>',methods['GET'])
+def quiz_results(quiz_id):
+    ## Find results for quiz
+    ## Order them by score
+    ## Convert to json
+    return json
+
+@app.route('/user/<int:user_id>', methods['GET'])
+def user_results(user_id):
+    ## Get Username
+    ## Get list of user results
+    ## Order them by date
+    ## Convert to json
+    return json
+
+@app.route('/question/<int:question_id>', methods['GET'])
+def get_question(question_id):
+    ## Get question
+    ## Convert to json
+    return json
+
+@app.route('/question', methods['GET'])
+def get_random_question():
+    ## Get random question
+    ## Convert to json
+
+## POST
+@app.route('/quiz/<int:quiz_id>', methods['POST'])
+def answer_quiz(quiz_id):
+    ## Get user name from request
+    ## Get answer from request
+    ## Find the next question to be answered
+    ## Answer quiz
+    ## Store response
+    ## Find out if that response is correct
+    return json
+
+@app.route('/quiz/create/', methods['POST'])
+def create_quiz():
+    ## Create quiz
+    ## Pick questions for that quiz
+    ## Join creator to quiz id
+    return json
+
+@app.route('/quiz/join/<int:quiz_id>', methods['POST'])
+def join_quiz(quiz_id):
+    ## Get user from request
+    ## Create responses for user
+    ## Store responses for user
+    print ""
+
+
+# TURN OFF DB
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
-
-
 
