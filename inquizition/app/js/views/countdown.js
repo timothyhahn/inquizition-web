1bcea17bbc994282b062982865b11beca6fb725e
Timothy Hahn <tyh25@drexel.edu>
2013-08-17 14:27:35 -0400

    Fixed really rare time bug

--- inquizition/app/js/views/countdown.js
+++ inquizition/app/js/views/countdown.js
@@ -10,7 +10,7 @@ var app = app || {};
       app.seconds = options.secondsLeft;
       app.decisecondsLeft = options.secondsLeft * 10;
       app.countDownInterval = window.setInterval(function () {
-        if(app.decisecondsLeft > 10) {
+        if(app.decisecondsLeft > 5 || app.decisecondsLeft > 1000) {
           app.decisecondsLeft--;
           deciTotal = app.seconds * 10;
           decisecondsTaken = deciTotal - app.decisecondsLeft;
